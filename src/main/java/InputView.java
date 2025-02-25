a7197dc9b134d557209f8d6d4ae91079d7199ccc
Jin409 <sualng123@gmail.com>
2025-02-25 14:08:15 +0900

    feat: 변경된 요구사항 반영

--- src/main/java/InputView.java
+++ src/main/java/InputView.java
@@ -4,7 +4,12 @@ public class InputView {
     private static final Scanner SCANNER = new Scanner(System.in);
 
     public static String readTotalPrice() {
-        System.out.println("주문 금액을 입력하세요: ");
+        System.out.print("주문 금액을 입력하세요: ");
+        return SCANNER.next();
+    }
+
+    public static String readCustomerType() {
+        System.out.print("고객 유형을 입력하세요 (GENERAL / VIP):");
         return SCANNER.next();
     }
 }
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,29 +1,56 @@
 public class Main {
     public static void main(String[] args) {
+        String rawCustomerType = InputView.readCustomerType();
         String rawOrderPrice = InputView.readTotalPrice();
+
+        CustomerType customerType = CustomerType.findByValue(rawCustomerType);
+
         int orderPrice = Integer.parseInt(rawOrderPrice);
-        int discountPrice = calculateDiscountPrice(orderPrice);
-        int deliveryFee = calculateDeliveryFee(orderPrice);
+        int discountPrice = calculateDiscountPrice(customerType, orderPrice);
+        int deliveryFee = calculateDeliveryFee(customerType, orderPrice);
 
         int totalOrderPrice = orderPrice - discountPrice + deliveryFee;
 
         OutputView.printResult(totalOrderPrice, discountPrice, deliveryFee);
     }
 
-    private static int calculateDiscountPrice(int orderPrice) {
-        int discountPrice = 0;
-        if (orderPrice >= 10_000) {
-            discountPrice += 1_000;
+    private static int calculateDiscountPrice(CustomerType customerType, int orderPrice) {
+        if (customerType == CustomerType.VIP) {
+            return calculateDiscountPriceForVIP(orderPrice);
         }
 
         if (orderPrice >= 50_000) {
-            discountPrice += 5_000;
+            return 5_000;
         }
-        return discountPrice;
+
+        if (orderPrice >= 10_000) {
+            return 1_000;
+        }
+
+        return 0;
     }
 
-    private static int calculateDeliveryFee(int orderPrice) {
+    private static int calculateDiscountPriceForVIP(int orderPrice) {
+
+        if (orderPrice >= 50_000) {
+            return 5_000;
+        }
+
+        if (orderPrice >= 10_000) {
+            return 2_000;
+        }
+
+        return 0;
+    }
+
+    private static int calculateDeliveryFee(CustomerType customerType, int orderPrice) {
+
         int deliveryFee = 0;
+
+        if (customerType == CustomerType.VIP) {
+            return 0;
+        }
+
         if (orderPrice < 10_000) {
             deliveryFee += 2_500;
         }
